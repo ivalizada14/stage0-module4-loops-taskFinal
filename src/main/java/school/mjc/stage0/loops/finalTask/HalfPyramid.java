0809cac6cf7e6490feaab1fadc6c7bfe5240ad65
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-02 16:03:23 +0300

    first

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for(int x = 1; x <= cathetusLength; x++)
+        {
+            for(int y = 1; y<=cathetusLength; y++)
+            {
+                if(y>=cathetusLength-x+1&&y<=cathetusLength+x-1)
+                    System.out.print('*');
+                else
+                    System.out.print(' ');
+            }
+            System.out.println();
 
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for(int x = 1; x <= cathetusLength; x++)
+        {
+            int numberToPrint = x;
+            for(int y = 1; y<cathetusLength * 2; y++)
+            {
+                if(y>=cathetusLength-x+1&&y<=cathetusLength+x-1)
+                {
+                    System.out.print(numberToPrint);
+                    if(y<cathetusLength)
+                        numberToPrint--;
+                    else
+                        numberToPrint++;
+                }
 
+                else{
+                    System.out.print(' ');
+                    //System.out.print(x);
+                    //System.out.print(' ');
+                    //System.out.print(y);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
